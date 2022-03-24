c75117c907105a9f505aaa329f7579197e88f6c4
Vedansh Agarwal <101228748+vedansh-agarwal@users.noreply.github.com>
2022-03-24 23:24:30 +0530

    Update IndianCurrecy.java

--- IndianCurrecy.java
+++ IndianCurrecy.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class IndianCurrecy
 {
-    public static void main()
+    public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number to be converted (without commas): ");
